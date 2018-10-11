0be534dcc978d9b2dc924f5cc604d794e04e03b9
Amit <amit.bhandari@outlook.in>
2018-10-11 08:54:52 +0530

    minor bug fixes

---
+++ app/src/main/java/com/music/player/bhandari/m/customViews/MoveUpwardBehavior.java
@@ -0,0 +1,39 @@
+package com.music.player.bhandari.m.customViews;
+
+import android.content.Context;
+import android.support.annotation.Keep;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+@Keep
+public class MoveUpwardBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public MoveUpwardBehavior() {
+        super();
+    }
+
+    public MoveUpwardBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.min(0, ViewCompat.getTranslationY(dependency) - dependency.getHeight());
+        ViewCompat.setTranslationY(child, translationY);
+        return true;
+    }
+
+    //you need this when you swipe the snackbar(thanx to ubuntudroid's comment)
+    @Override
+    public void onDependentViewRemoved(CoordinatorLayout parent, View child, View dependency) {
+        ViewCompat.animate(child).translationY(0).start();
+    }
+}
